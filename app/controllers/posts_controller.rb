795c7571892c721753b4706d1dfdcf253c814a1c
enoughtime@gmail.com <enoughtime@gmail.com>
2016-07-02 12:58:55 -0400

    “Checkpoint-32-Rails-CRUD-completed”

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,39 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.each_with_index do |post, index|
+      if index == 0 or index % 5 == 0
+        post.title = "SPAM"
+      end
+    end
   end
 
   def show
+    # #19
+    @post = Post.find(params[:id])
   end
 
   def new
+  # #7
+    @post = Post.new
+  end
+
+  def create
+# #9
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+# #10
+    if @post.save
+# #11
+      flash[:notice] = "Post was saved successfully."
+      redirect_to @post
+    else
+# #12
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :new
+    end
   end
 
   def edit
